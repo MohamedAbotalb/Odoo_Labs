e76ca2e66851f7c8a6b51efd235fa3d70e70f69f
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-05-02 23:39:34 +0300

    Add Day 1 lab files

---
+++ Day 1/hms/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Hospital Management System",
+    "author": "Mohamed Abotalb",
+    "website": "http://localhost:8069",
+    "description": "This is an application for hospital and patients data management",
+    "data": [
+        'views/hms_patient.views.xml'
+    ]
+}
---
+++ Day 1/hms/models/hms_patient.py
@@ -0,0 +1,26 @@
+from odoo import models, fields
+    
+    
+class Patient(models.Model):
+    _name = 'hms.patient'
+
+    first_name = fields.Char(string='First Name', required=True)
+    last_name = fields.Char(string='Last Name', required=True)
+    birth_date = fields.Date(string='Birth Date')
+    history = fields.Html(string='History')
+    cr_ratio = fields.Float(string='CR Ratio')
+    blood_type = fields.Selection([
+        ('A+', 'A+'),
+        ('A-', 'A-'),
+        ('B+', 'B+'),
+        ('B-', 'B-'),
+        ('AB+', 'AB+'),
+        ('AB-', 'AB-'),
+        ('O+', 'O+'),
+        ('O-', 'O-'),
+    ], string='Blood Type')
+    pcr = fields.Boolean(string='PCR')
+    image = fields.Binary(string='Image')
+    address = fields.Text(string='Address')
+    age = fields.Integer(string='Age')
+   
